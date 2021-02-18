208d2fdb3d9fe1cdff4065c7f2877651259a81d3
Dean Harris <dean@harris.tc>
2021-02-18 14:45:01 +1000

    feat(build): add some initial multi tool handling

--- src/commands/build.js
+++ src/commands/build.js
@@ -1,12 +1,24 @@
 const { spawn } = require('child_process');
-const { getBinary, getConfig } = require('../utils');
+const { getBinary, getConfig, getDeps } = require('../utils');
+
+const knownBuildTools = ['webpack', 'rollup'];
 
 const build = async () => {
   const args = process.argv.slice(3);
-  const webpackPath = await getBinary('webpack');
-  const config = getConfig('webpack');
+  const deps = await getDeps();
+  const foundBuildTool = knownBuildTools.filter((tool) => deps.includes(tool));
+
+  if (foundBuildTool.length !== 1) {
+    const errorMessage = foundBuildTool.length
+      ? 'Multiple build tools found in project dependencies'
+      : 'No build tool found in project dependencies';
+    throw new Error(errorMessage);
+  }
+
+  const buildToolPath = await getBinary(foundBuildTool[0]);
+  const config = await getConfig(oundBuildTool[0]);
   args.unshift('--config', config);
-  return spawn(webpackPath, args);
+  return spawn(buildToolPath, args);
 };
 
 module.exports = build;
