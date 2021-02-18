bfd80cbfb9aad6e7a9881af4c16f03b612836c5b
Dean Harris <dean@harris.tc>
2021-02-18 14:47:08 +1000

    feat: add help text and run entry async

--- index.js
+++ index.js
@@ -2,12 +2,51 @@ const commands = require('./src/commands');
 
 const [_, __, cmd] = process.argv;
 
-if (commands.hasOwnProperty(cmd)) {
-  const job = commands[cmd]();
-  job.on('close', (code, signal) => console.log(code, signal));
-  job.on('exit', (code, signal) => console.log(code, signal));
-  job.on('error', (err) => console.log(err));
-  job.on('message', (message, sendHandle) => console.log(message, sendHandle));
-} else {
-  // print some usage stuff
-}
+const helpText = `
+@deanacus/scripts
+
+DESCRIPTION:
+  A zero config development script toolkit for use with Node.js projects
+
+USAGE:
+  @deanacus/scripts <command>
+
+COMMANDS:
+  build
+  coverage
+  format
+  lint
+  start
+  test`;
+
+const main = async () => {
+  if (commands.hasOwnProperty(cmd)) {
+    try {
+      const job = await commands[cmd]();
+      job.addListener('close', (exitCode) => {
+        if (!exitCode) {
+          process.stdout.write('Success!');
+        }
+      });
+      job.addListener('error', (err) => {
+        process.stderr.write(err.toString());
+        let message = '';
+        if (err.path) {
+          message += err.path;
+        }
+        if (err.spawnargs) {
+          message += ` ${err.spawnargs.join(' ')}`;
+        }
+        console.log(message);
+        process.exit(1);
+      });
+    } catch (error) {
+      process.stdout.write(error.toString());
+    }
+  } else {
+    process.stdout.write(helpText);
+    process.exit(1);
+  }
+};
+
+main();
