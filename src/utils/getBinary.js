d188e7ea9a2e4d644d5a14007c967147c0ca4e44
Dean Harris <dean@harris.tc>
2021-02-18 14:44:06 +1000

    fix: return path to bin, not boolean in getBinary

--- src/utils/getBinary.js
+++ src/utils/getBinary.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 const path = require('path');
-const pkgDir = require('pkg-up');
+const pkgDir = require('pkg-dir');
 
 const getBinary = async (module) => {
   const rootDir = await pkgDir(process.cwd());
   const binDir = path.resolve(rootDir, './node_modules/.bin');
   return new Promise((resolve, reject) => {
     fs.access(`${binDir}/${module}`, fs.F_OK, (err) =>
-      err ? reject(err) : resolve(true),
+      err ? reject(err) : resolve(`${binDir}/${module}`),
     );
   });
 };
